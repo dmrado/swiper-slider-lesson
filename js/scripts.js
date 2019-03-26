8230724baa7529d960ff095bca07db95ee31516c
rdv0 <rdv0@ya.ru>
2019-03-26 21:23:56 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+﻿/* JavaScript */
+
+	/* swiper slider */
+	$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+		// Optional parameters
+		autoplay: {
+			delay: 5000,
+			},
+		pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				bulletElement: 'span',
+				bulletClass: 'swiper-pagination-bullets',
+				bulletActiveClass: 'swiper-pagination-bullet-active',
+				clickable: true
+			},
+		navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+		mousewheel: {
+			invert: true,
+			},
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+			},
+		coverflowEffect: {
+			rotate: 30,
+			slideShadows: false,
+			},
+		loop: true
+		})
+	});
